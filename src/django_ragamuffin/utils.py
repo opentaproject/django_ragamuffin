5e6134461be0de528f0b36270e7dd956afbbe8c0
Stellan Ostlund <stellan.ostlund@gmail.com>
2025-09-30 00:49:31 +0200

    Fix utils to improve mathjax

--- src/django_ragamuffin/utils.py
+++ src/django_ragamuffin/utils.py
@@ -61,6 +61,12 @@ def mathfix( txt ):
     txt = re.sub(r'(\w\')!', r'\1',txt)  # SHOULD NOT BE HERE
     txt = re.sub(r'(\w\')\!', r'\1',txt) # SHOULD NOT BE HERE
     txt = re.sub(r'(\w\')\\!', r'\1',txt)
+    txt = re.sub(r"operatorname","bold",txt )
+    txt = re.sub(r"boldsymbol","bold",txt )
+    txt = re.sub(r"\$\$(.*?)\$\$", r"<p/><p/>$\1$<p/><p/>", txt, flags=re.S)
+    txt = re.sub(r"\\dots", r"\\ldots", txt )
+    txt = re.sub(r'fileciteturn0file[0-9]+\.', '', txt )
+
     return txt
 
 
