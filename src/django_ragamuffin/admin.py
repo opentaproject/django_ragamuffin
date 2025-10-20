c527908ded2e30acada88277a93ac7591d81ac51
Stellan Ostlund <stellan.ostlund@gmail.com>
2025-10-21 01:41:57 +0200

    Codex fixes up admin.py

--- src/django_ragamuffin/admin.py
+++ src/django_ragamuffin/admin.py
@@ -2,13 +2,28 @@ from django.contrib import admin
 from django.db.models import Count, OuterRef, Subquery
 from django.conf import settings
 from django import forms
-from .models import OpenAIFile  , VectorStore , Assistant, Thread, DEFAULT_INSTRUCTIONS, RemoteVectorStore, QUser, Mode, ModeChoice, Message
+from .models import (
+    OpenAIFile,
+    VectorStore,
+    Assistant,
+    Thread,
+    RemoteVectorStore,
+    QUser,
+    Mode,
+    ModeChoice,
+    Message,
+)
 
 
 @admin.register(RemoteVectorStore)
 class RemoteVectorStoreAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'checksum' ,'list_file_names', 'vector_store_id', 'vector_stores_pks', )
-    list_display = ('pk', 'vector_store_id','checksum','list_file_names', 'vector_stores_pks', )
+    list_display = (
+        'pk',
+        'vector_store_id',
+        'checksum',
+        'list_file_names',
+        'vector_stores_pks',
+    )
 
     def list_file_names(self, obj):
         return ", ".join(obj.file_names())
@@ -19,15 +34,10 @@ class RemoteVectorStoreAdmin(admin.ModelAdmin):
 
 @admin.register(QUser)
 class QUserAdmin(admin.ModelAdmin):
-
-    def messages(self,obj):
-        return len( obj.messages() )
-
-    list_display = ('pk', 'username', 'is_staff','messages')
+    list_display = ('pk', 'username', 'is_staff', 'messages')
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        # annotate each user with a count of related messages
         return qs.annotate(message_count=Count('thread__thread_messages'))
 
     @admin.display(ordering='message_count')
@@ -44,7 +54,7 @@ class OpenAIFileAdmin(admin.ModelAdmin):
 
 @admin.register(VectorStore)
 class VectorStoreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'vsid', 'checksum', 'list_file_ids')  # Add your custom method here
+    list_display = ('id', 'name', 'vsid', 'checksum', 'list_file_ids')
     readonly_fields = ('checksum','vsid')
 
     def get_readonly_fields(self, request, obj=None):
@@ -58,24 +68,38 @@ class VectorStoreAdmin(admin.ModelAdmin):
     list_file_ids.short_description = "File Names"
     
 class AssistantForm(forms.ModelForm):
-
-    actual_instructions = forms.CharField(disabled=True, required=False, widget=forms.Textarea(attrs={'disabled': 'disabled'}),)
+    actual_instructions = forms.CharField(
+        disabled=True,
+        required=False,
+        widget=forms.Textarea(attrs={'disabled': 'disabled'}),
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        instance = self.instance
-        # Set initial value for the readonly field
-        #self.fields['actual_instructions'].initial = instance.get_instructions() + ' '.join( DEFAULT_INSTRUCTIONS.split() )  if self.instance.pk else "N/A"
-        if self.instance.pk :
-            instructions = ' '.join( instance.get_instructions().split() );
-        self.fields['actual_instructions'].initial = instructions if self.instance.pk else "N/A"
+        if self.instance.pk:
+            instructions = ' '.join(self.instance.get_instructions().split())
+        else:
+            instructions = "N/A"
+        self.fields['actual_instructions'].initial = instructions
 
     class Meta:
         model = Assistant
-        fields = ['name','mode_choice','instructions','vector_stores','assistant_id','json_field','temperature','actual_instructions' ,]
+        fields = [
+            'name',
+            'mode_choice',
+            'instructions',
+            'vector_stores',
+            'assistant_id',
+            'json_field',
+            'temperature',
+            'actual_instructions',
+        ]
         help_texts = {
             'temperature': f"Default temperature = {settings.DEFAULT_TEMPERATURE}",
-            'instructions' : f"Leave blank for default; start with 'append: XXX...' to append 'XXX...' to default; Any other non-blank string completely replaces the default instructions.'"
+            'instructions': (
+                "Leave blank for default; start with 'append: XXX...' to append 'XXX...' "
+                "to default; Any other non-blank string completely replaces the default instructions.'"
+            ),
         }
 
 
@@ -105,7 +129,6 @@ class AssistantAdmin(admin.ModelAdmin):
     form = AssistantForm
     list_display = ('id', 'name', 'mode_choice', 'assistant_id', 'subdomain', 'file_names', 'file_pks')
     list_filter = (AssistantSubdomainListFilter,)
-    #list_display = ('id', 'name', 'mode_choice', 'assistant_id', 'file_names','file_pks', 'file_ids', 'remote_files','list_vector_store_ids')  # Add your custom method here
 
     def list_vector_store_ids(self, obj):
         return ", ".join(str(f.name ) for f in obj.vector_stores.all())
@@ -142,9 +165,8 @@ class MyMessageForm(forms.ModelForm):
 
 @admin.register(Thread)
 class ThreadAdmin(admin.ModelAdmin):
-    form = MyThreadForm;
+    form = MyThreadForm
     list_display = ('id', 'name', 'user', 'thread_id', 'assistant', 'subdomain', 'messages')
-    list_filter = ()
 
     class ThreadSubdomainListFilter(admin.SimpleListFilter):
         title = 'subdomain'
@@ -191,7 +213,7 @@ class ThreadAdmin(admin.ModelAdmin):
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     form = MyMessageForm
-    list_display = ('id',  'thread', 'subdomain')  # Add your custom method here
+    list_display = ('id', 'thread', 'subdomain')
     list_filter = ('subdomain',)
 
 
