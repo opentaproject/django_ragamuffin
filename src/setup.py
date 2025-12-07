466ec4daf078a8de5e59847a1ff3add5133572ac
Stellan Ostlund <stellan.ostlund@gmail.com>
2025-12-08 00:54:43 +0100

    Set to v22

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='django_ragamuffin',  
-    version='1.107.1.21',
+    version='1.107.1.22',
     packages=find_packages(),
     include_package_data=True,  # Important for static files/migrations
     license='MIT License',  # Choose your license
