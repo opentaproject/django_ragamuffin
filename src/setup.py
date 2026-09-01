062b9b716bc100179e66b0c18976e2c8d3bf6484
Stellan Ostlund <opentaproject@gmail.com>
2026-09-01 09:22:25 +0200

    Fix typo in setup

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='django_ragamuffin',  
-    version='3.60.0.1',
+    version='3.6.0.1',
     packages=find_packages(),
     include_package_data=True,  # Important for static files/migrations
     license='MIT License',  # Choose your license
