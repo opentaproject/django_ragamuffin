aa63d1fc7033152ef459000243f526b940b666f1
Stellan Ostlund <stellan.ostlund@gmail.com>
2026-09-05 12:48:50 +0200

    Bump to 3.6.0.3

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='django_ragamuffin',  
-    version='3.6.0.2',
+    version='3.6.0.3',
     packages=find_packages(),
     include_package_data=True,  # Important for static files/migrations
     license='MIT License',  # Choose your license
